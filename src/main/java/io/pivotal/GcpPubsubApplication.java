99ef6e7775820bc0752b35e2678e40f152f14995
Michael Goddard <mgoddard@gopivotal.com>
2017-02-01 06:24:06 -0500

    Cleaning up some experimental/debugging noise

--- src/main/java/io/pivotal/GcpPubsubApplication.java
+++ src/main/java/io/pivotal/GcpPubsubApplication.java
@@ -31,12 +31,8 @@ public class GcpPubsubApplication {
 	 * https://github.com/GoogleCloudPlatform/google-cloud-java/tree/master/google-cloud-pubsub
 	 * https://github.com/GoogleCloudPlatform/google-cloud-java/issues/1430
 	 *
-	 * Caveats:
+	 * Caveat(s):
 	 * (1) If running via IntelliJ, you will get a "DEADLINE_EXCEEDED" error (just run on command line)
-	 * (2) If you use the PubSubOptions.newBuilder()... approach (below), you may just hit a
-	 *     "PERMISSION_DENIED" error.  For now, the only way I could get past that was to use the
-	 *     alternative, PubSubOptions.getDefaultInstance().getService()
-	 *
 	 */
     @Bean
     public PubSub pubSubCloud(
@@ -48,15 +44,8 @@ public class GcpPubsubApplication {
         //logger.info("JSON: " + json);
         InputStream in = new ByteArrayInputStream(Base64.getDecoder().decode(privateKeyData));
         Credentials cred = ServiceAccountCredentials.fromStream(in);
-        // The approach below, the newBuilder()... way, fails with "PERMISSION_DENIED" errors
         rv = PubSubOptions.newBuilder().setProjectId(projectId).setCredentials(cred).build().getService();
         logger.info("Just got a new PubSub instance");
-        // Ref. for below: https://github.com/GoogleCloudPlatform/python-docs-samples/issues/697
-        /*
-         * For the approach below to work, you must dump that JSON (see above) into a file, then
-         * set the environment variable GOOGLE_APPLICATION_CREDENTIALS to the location of the file.
-         */
-        //rv = PubSubOptions.getDefaultInstance().getService();
         return rv;
     }
 
