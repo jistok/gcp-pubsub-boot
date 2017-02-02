fbcc33028b3071d7ccc4565ef8b2b97067691d74
Michael Goddard <mgoddard@gopivotal.com>
2017-02-02 13:15:32 -0500

    Commenting the 'pubTime' field since I wasn't sure on that

--- src/main/java/io/pivotal/PubSubMessage.java
+++ src/main/java/io/pivotal/PubSubMessage.java
@@ -3,7 +3,7 @@ package io.pivotal;
 public class PubSubMessage {
 
     private final String id;
-    private final long pubTime;
+    private final long pubTime; // Units: milliseconds since 00:00:00 UTC, January 1, 1970
     private final String content;
 
     public PubSubMessage(String id, long pubTime, String content) {
